f14ea533778d4c38705023d93b671b569692883e
John Ashmore <john@xforge.co.uk>
2016-07-14 11:01:38 +0100

    Add unit tests.

---
+++ app/my-uppercase.pipe.spec.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by john on 14/07/2016.
+ */
+import { MyUppercasePipe } from './my-uppercase.pipe';
+describe('MyUppercasePipe', () => {
+  let pipe: MyUppercasePipe;
+  beforeEach(() => {
+    pipe = new MyUppercasePipe();
+  });
+  it('transforms "abc" to "ABC"', () => {
+    expect(pipe.transform('abc')).toEqual('ABC');
+  });
+  it('transforms "abc def" to "ABC DEF"', () => {
+    expect(pipe.transform('abc def')).toEqual('ABC DEF');
+  });
+  it('leaves "ABC DEF" unchanged', () => {
+    expect(pipe.transform('ABC DEF')).toEqual('ABC DEF');
+  });
+});
