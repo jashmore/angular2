2e6e0406e672d8698f2978834d8da936cb0a0867
John Ashmore <john@xforge.co.uk>
2018-10-08 16:30:12 +0100

    Updated to new version of tutorial.

--- src/app/1st.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('1st test - this is the title...', () => {
-  it('true is true', () => expect(true).toBe(true));
-});
--- src/app/hero.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by john on 19/03/17.
- */
-export class Hero {
-  id: number;
-  name: string;
-}
